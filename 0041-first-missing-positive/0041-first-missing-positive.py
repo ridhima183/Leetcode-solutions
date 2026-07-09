c06844f7d79660a01e23edcfdb3c4487ef4caa7c
ridhima183 <ridhima.gadalay@gmail.com>
2026-07-09 23:35:47 +0530

    Time: 33 ms (70.83%), Space: 35 MB (7.92%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -5,11 +5,10 @@ class Solution:
         for i in range(n):
             if nums[i]>0:
                 hash_set.add(nums[i])
-                
         if not hash_set :
             return 1
 
-        for i in range(1,max(nums)+1):
+        for i in range(1,len(nums)+1):
             if i not in hash_set:
                 return i 
         return (max(nums)+1)
